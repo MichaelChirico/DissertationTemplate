8bb35842cc9409ace6ca01af954521e4253afb7d
Michael Chirico <MichaelChirico4@gmail.com>
2017-05-03 10:10:26 -0400

    closer to automating assembly

--- aggregator.sh
+++ aggregator.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # pull latest version of papers and figures to the dissertation folder
 
+# EXTRACT CHAPTER 1
 # directory where to find chapter 1
 ch1wd='/home/michael/Desktop/research/Wisconsin Bargaining/analysis_code'
-# copy .tex of chapter 1 to content folder here
-cp "$ch1wd"/turnover_paper.tex ./content
+# copy .tex of chapter 1 and references to full chapter folder here
+cp "$ch1wd"/turnover_paper.tex "$ch1wd"/references.bib ./full_chapters
 # copy figures of chapter 1 to figures folder here
+#   -- for knitr users, set fig.path = 'figures/' in your chunk options
 cp "$ch1wd"/figures/* ./figures
+
+# EXTRACT CHAPTER 2
+
+# ASSEMBLE
+Rscript body_extractor.R
--- body_extractor.R
+++ body_extractor.R
@@ -1,18 +1,16 @@
 #!/bin/Rscript
 # CHAPTER 1: TURNOVER
-ch1_dir = 
-  paste0('~/Desktop/research/Wisconsin Bargaining/analysis_code/')
-ch1 = readLines(paste0(ch1_dir, 'turnover_paper.tex'))
-body_start = grep('\\maketitle', ch1, fixed = TRUE) + 1L
+ch1 = readLines('full_chapters/turnover_paper.tex')
+body_start = grep('\\TAG{BEGIN_BODY}', ch1, fixed = TRUE) + 1L
 #bibliography included at end of dissertation
-body_end = grep('\\section*{References}', ch1, fixed = TRUE) - 1L
-writeLines(ch1[body_start:body_end], 'content/sail_chapter.tex')
+body_end = grep('\\TAG{END_BODY}', ch1, fixed = TRUE) - 1L
+writeLines(ch1[body_start:body_end], 'content/turnover.tex')
 
 # CHAPTER 2: 
 
 # CONCATENATE INDIVIDUAL REFERENCES
 ## (note designed to deal with duplicated references...
 ##  maybe see bibtex or RefManageR packages)
-full_bib = c(readLines(paste0(ch1_dir, 'references.bib')))
+full_bib = c(readLines('full_chapters/references.bib'))
 
 writeLines(full_bib, 'references.bib')
