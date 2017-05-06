2584787128827e9c3daceadc4fad74fa567c5cd7
Michael Chirico <MichaelChirico4@gmail.com>
2017-05-05 21:00:46 -0400

    updates

--- aggregator.sh
+++ aggregator.sh
@@ -6,11 +6,19 @@
 ch1wd='/home/michael/Desktop/research/Wisconsin Bargaining/analysis_code'
 # copy .tex of chapter 1 and references to full chapter folder here
 cp "$ch1wd"/turnover_paper.tex "$ch1wd"/references.bib ./full_chapters
+# rename references with chapter suffix
+mv full_chapters/references.bib full_chapters/references_turnover.bib
 # copy figures of chapter 1 to figures folder here
 #   -- for knitr users, set fig.path = 'figures/' in your chunk options
 cp "$ch1wd"/figures/* ./figures
 
 # EXTRACT CHAPTER 2
+
+# EXTRACT CHAPTER 3
+ch3wd='/home/michael/Desktop/research/Sieg_LMI_Real_Estate_Delinquency/round_two'
+cp "$ch3wd"/round_two_paper.tex "$ch3wd"/references.bib ./full_chapters
+mv full_chapters/references.bib full_chapters/references_procrastination.bib
+# no figures in chapter 3
 
 # ASSEMBLE
 Rscript body_extractor.R
--- body_extractor.R
+++ body_extractor.R
@@ -6,11 +6,21 @@ body_start = grep('\\TAG{BEGIN_BODY}', ch1, fixed = TRUE) + 1L
 body_end = grep('\\TAG{END_BODY}', ch1, fixed = TRUE) - 1L
 writeLines(ch1[body_start:body_end], 'content/turnover.tex')
 
-# CHAPTER 2: 
+# CHAPTER 3: PROCRASTINATION
+ch3 = readLines('full_chapters/round_two_paper.tex')
+body_start = grep('\\TAG{BEGIN_BODY}', ch3, fixed = TRUE) + 1L
+body_end = grep('\\TAG{END_BODY}', ch3, fixed = TRUE) - 1L
+writeLines(ch3[body_start:body_end], 'content/procrastination.tex')
+
+apdx_start = grep('\\TAG{BEGIN_APPENDIX}', ch3, fixed = TRUE) + 1L
+apdx_end = grep('\\TAG{END_APPENDIX}', ch3, fixed = TRUE) - 1L
+writeLines(ch3[apdx_start:apdx_end], 'content/appendix_procrastination.tex')
 
 # CONCATENATE INDIVIDUAL REFERENCES
 ## (note designed to deal with duplicated references...
 ##  maybe see bibtex or RefManageR packages)
-full_bib = c(readLines('full_chapters/references.bib'))
+full_bib = unlist(lapply(c('full_chapters/references_turnover.bib',
+                           'full_chapters/references_procrastination.bib'),
+                         readLines))
 
 writeLines(full_bib, 'references.bib')
